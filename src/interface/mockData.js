3dd97d467396959304eea585964f4d590af1d7ac
jerryxu <13048699388@163.com>
2018-08-07 16:13:08 +0800

    装饰器

--- src/interface/mockData.js
+++ src/interface/mockData.js
@@ -1,10 +1,10 @@
 export default {
   getRequestBaseParams:{
     TOKEN_ID:"111111111111111111",
-    APPVERSION:"4.0.1",
-    OSVERSION:"1111",
-    PLATFORM:"IOS",
-    channelNo:"111"
+    APP_VERSIONS:"4.0.1",
+    PHONE_VERSIONS:"1111",
+    PHONE_PLATFORM:"IOS",
+    CHANNEL_NO:"111"
 
   },
   nativeOcrFunc:{

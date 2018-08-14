620e2a7f0a9e41df0491e0ff563694eabf40f402
jerryxu <13048699388@163.com>
2018-08-14 10:23:25 +0800

    邮箱账单导入同步页面

--- src/page/cyber/CyberBank.jsx
+++ src/page/cyber/CyberBank.jsx
@@ -5,7 +5,7 @@ import ModalCom from "../../compoents/ModalCom";
 import {Tabs,Toast} from "antd-mobile"
 import {checkToken, getLoginList, loginCyber, verifyCode, pollingCyber} from "../../actions/reqAction";
 import {InitDecorator} from "../../compoents/InitDecorator";
-const prompt = Modal.prompt;
+
 
 const cardType = 'CREDITCARD'
 
@@ -62,146 +62,13 @@ export default class CyberBank extends React.Component {
       // 任务创建失败
       Toast.info('任务创建失败', 1);
     } else {
-      setTimeout(() => {
-        this.loopLogin(taskId,loginType);
-      }, 3000)
+      this.props.history.push('/load/cyber',{
+        taskId,loginType:'01'
+      })
+
     }
   }
 
-  waitFunc(time){
-    return new Promise((resolve,reject)=>{
-      setTimeout(()=>{
-        resolve()
-      },time)
-    })
-  }
-
-  /**
-   *   @author jerryxu
-   *   @params 用户信息
-   *   @description 第二步流程 检查任务状态并获取任务状态
-   */
-  async loopLogin(taskId,loginType){
-
-    let status = {};
-    do {
-
-      const reqParams = await this.props.getBaseParams();
-      status = await this.props.dispatch(checkToken({
-        taskId,
-        ...reqParams
-      }))
-    } while (this.judgeStatus(status))
-    this.handleStatus(status, taskId,loginType);
-  }
-
-  /**
-   *   @author jerryxu
-   *   @params 用户信息
-   *   @description 第三步流程判断任务状态，分别处理
-   */
-  handleStatus(status, taskId, loginType) {
-    const { data = {} } = status;
-    const { phase, phase_status ='', input ,description } = data;
-    switch (phase_status) {
-      case 'WAIT_CODE'://输入验证码
-
-        return this.promptClick({
-          input,
-          description,
-          taskId,
-          callback:this.verifycation.bind(this)
-        })
-      case "DOING":
-        return;
-      case "DONE_SUCC"://成功登录
-        this.props.history.push('/load/cyber',{
-            taskId,loginType
-        })
-        return;
-      case "DONE_FAIL":
-        Toast.info(description);
-        this.setDeepState('inputData',{login_type:loginType},{
-          disabled:true
-        });
-        return;
-      case "DONE_TIMEOUT":
-        return;
-      default:
-        return;
-    }
-  }
-
-
-
-
-  /**
-  *   @author jerryxu
-  *   @methodName
-  *   @params 任务编号 短信验证码
-  *   @description 第三步流程的分支流程，输入验证码检查登录状态
-  */
-  async verifycation({taskId,value:code}){
-    let codeStatus = ''
-
-    const reqParams = await this.props.getBaseParams();
-    codeStatus = await this.props.dispatch(verifyCode({
-      taskId,
-      code,
-      ...reqParams
-    }));
-
-    const { data } = codeStatus;
-    //if(value == '200'){
-
-    //} else {
-    this.loopLogin(taskId);
-    //}
-  }
-
-  /**
-  *   @author jerryxu
-  *   @methodName i
-  *   @params 魔蝎验证码模版 任务编号 信息描述 回调函数
-  *   @description Popup提示，输入信息，异步处理
-  */
-  promptClick({ input, taskId, description, callback}){
-    prompt('输入验证码', description, [{
-      text: '取消',
-      onPress: value => new Promise((resolve) => {
-        Toast.info('登录失败', 1);
-        setTimeout(() => {
-          resolve();
-          console.log(`value:${value}`);
-        }, 1000);
-      }),
-    },
-      {
-        text: '确定',
-        onPress: value => new Promise((resolve, reject) => {
-          callback({taskId,value})
-          resolve();
-          // setTimeout(() => {
-          //   this.setState({modal: true, description: "您绑定的卡为借记卡，卡包只支持绑定信用卡，请您重新绑定"})
-          //   console.log(`value:${value}`);
-          // }, 1000);
-        }),
-      },
-    ], 'default', null, ['请输入验证码'])
-
-  }
-
-
-  judgeStatus(status) {
-    const { data } = status;
-    const { phase, phase_status } = data;
-    switch (phase_status) {
-      case "DOING":
-        return true;
-      default:
-        return false;
-    }
-  }
 
   //For Object
   setDeepState(property,key,obj) {
--- src/page/result/Loading.jsx
+++ src/page/result/Loading.jsx
@@ -1,9 +1,10 @@
 import React from 'react';
 import Header from "../../compoents/Header";
-import { pollingCyber} from "../../actions/reqAction";
+import {checkEmailTask, checkToken, pollingCyber, verifyCode} from "../../actions/reqAction";
 import {InitDecorator} from "../../compoents/InitDecorator";
 import Loading from "../../compoents/Loading";
-import { Toast } from 'antd-mobile';
+import { Toast,Modal } from 'antd-mobile';
+const prompt = Modal.prompt;
 
 const results = {
   cyber:{
@@ -28,8 +29,147 @@ export default class LoadingStatus extends React.Component{
 
   /**
    *   @author jerryxu
+   *   @methodName i
+   *   @params mod
+   *   @description 第二步流程
+   */
+  async checkTask(taskId,) {
+    const reqParams = await this.props.getBaseParams();
+    const {
+      state = {}
+    } = this.props.location;
+    const {
+      loginType
+    } = state;
+
+    let login;
+
+    do {
+      if(loginType == '03'){
+        login = await this.props.dispatch(checkEmailTask({
+          ...reqParams,
+          taskId
+        }))
+      } else if(loginType == '01'){
+        login = await this.props.dispatch(checkToken({
+          taskId,
+          ...reqParams
+        }))
+      }
+      debugger;
+    } while (this.judgeStatus(login))
+
+
+    this.handleStatus(login, taskId, loginType)
+
+  }
+
+
+  judgeStatus(status) {
+    const {data} = status;
+    const {phase, phase_status} = data;
+    switch (phase_status) {
+      case "DOING":
+        return true;
+      default:
+        return false;
+    }
+  }
+
+  /**
+   *   @author jerryxu
    *   @params 用户信息
-   *   @description 检查邮箱导入状态
+   *   @description 第三步流程判断任务状态，分别处理
+   */
+  handleStatus(status, taskId, loginType) {
+    const {data = {}} = status;
+    const {phase, phase_status = '', input, description} = data;
+    switch (phase_status) {
+      case 'WAIT_CODE'://输入验证码
+
+        return this.promptClick({
+          input,
+          description,
+          taskId,
+          callback: this.verifycation.bind(this)
+        })
+      case "DOING":
+        return;
+      case "DONE_SUCC"://成功登录
+
+        this.loopCheckAlways({taskId,loginType})
+        return;
+      case "DONE_FAIL":
+        Toast.info(description);
+        return;
+      case "DONE_TIMEOUT":
+        return;
+      default:
+        return;
+    }
+  }
+
+  /**
+   *   @author jerryxu
+   *   @methodName
+   *   @params 任务编号 短信验证码
+   *   @description 第三步流程的分支流程，输入验证码检查登录状态
+   */
+  async verifycation({taskId, value: code}) {
+    let codeStatus = ''
+
+    const reqParams = await this.props.getBaseParams();
+    codeStatus = await this.props.dispatch(verifyCode({
+      taskId,
+      code,
+      ...reqParams
+    }));
+
+    const {data} = codeStatus;
+    //if(value == '200'){
+
+    //} else {
+    this.checkTask(taskId);
+    //}
+  }
+
+
+  /**
+   *   @author jerryxu
+   *   @methodName i
+   *   @params 魔蝎验证码模版 任务编号 信息描述 回调函数
+   *   @description Popup提示，输入信息，异步处理
+   */
+  promptClick({input, taskId, description, callback}) {
+    prompt('输入验证码', description, [{
+      text: '取消',
+      onPress: value => new Promise((resolve) => {
+        Toast.info('登录失败', 1);
+        setTimeout(() => {
+          resolve();
+          console.log(`value:${value}`);
+        }, 1000);
+      }),
+    },
+      {
+        text: '确定',
+        onPress: value => new Promise((resolve, reject) => {
+          callback({taskId, value})
+          resolve();
+          // setTimeout(() => {
+          //   this.setState({modal: true, description: "您绑定的卡为借记卡，卡包只支持绑定信用卡，请您重新绑定"})
+          //   console.log(`value:${value}`);
+          // }, 1000);
+        }),
+      },
+    ], 'default', null, ['请输入验证码'])
+
+  }
+
+  /**
+   *   @author jerryxu
+   *   @params 用户信息
+   *   @description 检查账单导入状态
    */
 
   async loopCheckAlways({ taskId,loginType }) {
@@ -46,6 +186,7 @@ export default class LoadingStatus extends React.Component{
           progress:this.state.progress+10
         })
       }
+      debugger;
 
     } while ( pollingStatus && typeof pollingStatus.data != 'undefined' && !pollingStatus.data)
     const {data} =pollingStatus
@@ -67,9 +208,7 @@ export default class LoadingStatus extends React.Component{
     const {
       taskId = "586b4860-9c76-11e8-89e7-00163e0dfac7",
     } = state;
-    this.loopCheckAlways({
-      taskId,
-    });
+    this.checkTask(taskId);
   }
   render() {
     const  { type,}   = this.props.match.params;

3c3748632d3671b120dea49937411ef72d13b9ad
jerryxu <13048699388@163.com>
2018-08-27 14:54:43 +0800

    [Fixed bug]实名认证

--- src/page/home/Index.jsx
+++ src/page/home/Index.jsx
@@ -130,9 +130,9 @@ export default class Index extends React.Component {
       } else if( authSts == '-1') {
         //数据尚未装载完毕不处理
       } else if(authSts == '99') {
-        alert('使用前请先进行实名认证','',[
+        alert('您尚未通过实名认证，请先进行实名认证','',[
           {text:"取消",onPress:()=>{},style: 'default'},
-          {text:"确定",onPress:()=>{jsNative.nativeGoRealName();},style: 'default'},
+          {text:"去认证",onPress:()=>{jsNative.nativeGoRealName();},style: 'default'},
         ])
       } else {
         jsNative.nativeGoRealName();
@@ -229,14 +229,14 @@ export default class Index extends React.Component {
             if(examineAccount && type == '1'){
               return null
             }
-            return <div key={k}>
-              <span style={styles.iconItem} onClick={() => {
-                if (type == '0') {
-                  this.loginEnter(3, {action})
-                } else if (type == '1') {
-                  this.loginEnter(4, {action})
-                }
-              }}>
+            return <div key={k} onClick={() => {
+              if (type == '0') {
+                this.loginEnter(3, {action})
+              } else if (type == '1') {
+                this.loginEnter(4, {action})
+              }
+            }}>
+              <span style={styles.iconItem} >
                 <img src={img} style={{width: '0.65rem'}}/>
               </span>
               <div style={styles.textStyle}>

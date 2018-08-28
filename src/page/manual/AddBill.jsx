b518a4fd517dc72a97e05064f14f3d094a3bade6
jerryxu <13048699388@163.com>
2018-08-28 14:34:55 +0800

    [Fixed bug]提示信息

--- src/page/manual/AddBill.jsx
+++ src/page/manual/AddBill.jsx
@@ -95,7 +95,7 @@ export default class AddBill extends React.Component {
       Toast.info('请填写信用额度');
       return;
     } else if(!newBalance){
-      Toast.info('请填写剩余额度');
+      Toast.info('请填写账单金额');
       return;
     }
     if(parseInt(creditLimit)<parseInt(newBalance)){

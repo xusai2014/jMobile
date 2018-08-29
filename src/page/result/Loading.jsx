3b7bcb214ea7935f05dc5334a214031c9ca8c785
jerryxu <13048699388@163.com>
2018-08-29 11:31:20 +0800

    [Fixed bug]中断流程

--- src/page/result/Loading.jsx
+++ src/page/result/Loading.jsx
@@ -276,6 +276,7 @@ export default class LoadingStatus extends React.Component{
       alert(<span className="alert_title">返回将会中断导入，确认继续吗？</span>,'',[
         {text:"取消",onPress:()=>{},style: globalStyle.cancelStyle},
         {text:"确认",onPress:()=>{
+          promiseList.cancel();
           window.history.go(-1);
         },style: globalStyle.sureStyle},
       ])

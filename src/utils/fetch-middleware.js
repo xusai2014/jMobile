7e62df88797a038621661e5bcf7bfc36da5117c0
jerryxu <13048699388@163.com>
2018-08-30 20:53:45 +0800

    [Fixed bug]alpha验证

--- src/utils/fetch-middleware.js
+++ src/utils/fetch-middleware.js
@@ -72,7 +72,7 @@ export const  fetchPromise = async (url, method = 'GET', data, cancel = false, i
   //let baseUrl = 'http://172.16.135.174:8080/phoneclient/notify.htm'; //开发服务器
   let baseUrl = apiUrl;//'http://172.16.42.28:8080/lemon-mobile/phoneclient/notify.htm'; //李建
   if(window.location.host.indexOf('mpmw.vbill.cn')>-1 ){
-    baseUrl = '//mp-alpha.vbill.cn/phoneclient/notify.htm'
+    baseUrl = 'https://mp-alpha.vbill.cn:8084/phoneclient/notify.htm'
   }  else if(window.location.host.indexOf('mpmw-rc.vbill.cn')>-1){
     baseUrl ='https://mp-rc.vbill.cn/phoneclient/notify.htm'
   } else if(window.location.host.indexOf('mpmw-alpha.vbill.cn')>-1){

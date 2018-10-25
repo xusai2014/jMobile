57a01e24f6435c7ec5717d3b8aa2c5472b72ad35
jerryxu <13048699388@163.com>
2018-10-25 15:49:16 +0800

    调整Header 头部

--- src/utils/fetch-middleware.js
+++ src/utils/fetch-middleware.js
@@ -203,9 +203,6 @@ export function parseJSON(response) {
  */
 export const headers = {
   "Content-Type": "application/json;charset=UTF-8",
-  'Access-Control-Allow-Origin': '*',
-  "Access-Control-Allow-Methods": "GET,POST,OPTIONS",
-  "Access-Control-Allow-Headers": "Content-Type,Accept,X-Requested-With,access-control-allow-methods,remember-me,Access-Control-Allow-Origin,access-control-allow-headers"
 }
 
 /**

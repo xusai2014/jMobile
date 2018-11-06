710fc27f68391db1f20d8fd5edb4772448ae59c2
jerryxu <13048699388@163.com>
2018-11-06 15:28:14 +0800

    update shence url

--- src/reactApp.jsx
+++ src/reactApp.jsx
@@ -53,7 +53,7 @@ nativeJs.informLoginStatus(() => {
  *  神策数据统计，初始化
  */
 sa.init({
-  server_url: process.env.mode != 'production' ? 'https://sc.vbill.cn/sa?project=MPOS_TEST' : 'https://sc.vill.cn/sa?project=SXF_PLUS',
+  server_url: process.env.mode != 'production' ? 'https://sc.vbill.cn/sa?project=MPOS_TEST' : 'https://sc.vbill.cn/sa?project=SXF_PLUS',
   heatmap: {
     //是否开启点击图，默认 default 表示开启，自动采集 $WebClick 事件，可以设置 'not_collect' 表示关闭
     clickmap: 'default',

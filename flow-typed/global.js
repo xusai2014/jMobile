869b6a3550eaa86e8f4042a5c130cc679a9d3f3b
jerryxu <13048699388@163.com>
2018-11-21 16:12:20 +0800

    flow refactor

--- flow-typed/global.js
+++ flow-typed/global.js
@@ -1,5 +1,8 @@
 /* eslint-disable */
 declare function moment(): any;
 declare var _ : any;
+declare var $ : any;
 declare var CryptoJS:any;
+declare var gloablMinHeight:string; // 页面的满屏高度
+
 declare type ActionType = Array<string>;
--- src/compoents/FixedContent.jsx
+++ src/compoents/FixedContent.jsx
@@ -1,30 +1,51 @@
-import React from 'react';
-import {withRouter} from 'react-router-dom'
+// @flow
+import * as React from 'react';
+import { withRouter } from 'react-router-dom';
 
-class FixedContent extends React.Component {
-
-    initStyle() {
-        const designedWidth = 750;
-        const actualWidth = Math.min(450, $(window).width());
-        window.requestAnimationFrame(()=> {
-          document.body.style.overflowY = '';
-          document.body.style.position = 'static';
-        })
-        document.documentElement.style.fontSize = actualWidth * 100 / designedWidth + 'px';
-    }
-
-    componentWillMount() {
-        this.initStyle()
-    }
-
-    render() {
-        var {className, children,} = this.props;
-        return (
-            <div ref="content" className={className} style={{background: '#F5F5F5', width:'7.5rem',minHeight:gloablMinHeight}}>
-                {children}
-            </div>
-        );
-    }
+type Props = {
+  className:string,
+  children:React.Node
 }
+type State = {}
 
-export default FixedContent = withRouter(FixedContent)
+@withRouter
+export default class FixedContent extends React.Component<Props, State> {
+  componentWillMount() {
+    this.initStyle();
+  }
+
+  componentDidMount() {
+    window.addEventListener('resize', this.initStyle);
+  }
+
+  componentWillUnmount() {
+    window.removeEventListener('resize', this.initStyle);
+  }
+
+  initStyle = () => {
+    const designedWidth = 750;
+    const actualWidth = Math.min(450, $(window).width());
+
+    window.requestAnimationFrame(() => {
+      if (document.body && document.body.style) {
+        document.body.style.overflowY = '';
+        document.body.style.position = 'static';
+      }
+    });
+    if(document.documentElement){
+      document.documentElement.style.fontSize = `${actualWidth * 100 / designedWidth}px`;
+    }
+  };
+
+  render() {
+    const { children, className } = this.props;
+    return (
+      <div
+        ref="content"
+        className={className}
+        style={{ background: '#F5F5F5', width: '7.5rem', minHeight: gloablMinHeight }}>
+        {children}
+      </div>
+    );
+  }
+}

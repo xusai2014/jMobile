c37c8b973c7d7dbbf2ebde56d5dab13e452fc2ba
杨丽姣 <yanglijiao7@gmail.com>
2019-02-14 14:15:57 +0800

    轮播图选择银行

--- src/page/home/Home.jsx
+++ src/page/home/Home.jsx
@@ -1,63 +1,102 @@
-import React, { Component } from 'react';
+import React, {Component} from 'react';
 import styles from './Home.less';
-import { Carousel } from 'antd-mobile';
+import {Carousel} from 'antd-mobile';
 import Header from '../../compoents/Header';
 import BankList from './components/BankList';
 import process from '../../../static/img/process.png'
 
 export default class Home extends Component {
   state = {
-    selected: false // 银行图标是否是选中状态
+    selected: false, // 银行图标是否是选中状态
+    bankName: ''// 选中的银行名称
   }
+
   componentDidMount() {
   }
-  chooseBank = () => {
-    const { selected } = this.state;
-    if (selected) this.setState({ selected: false });
-    else this.setState({ selected: true });
+
+  // 选择相应的银行
+  chooseBank = (name) => {
+    const {bankName} = this.state;
+    console.log('name', name);
+    this.setState({
+      bankName: name
+    });
+    console.log('bankName', bankName);
+    const {selected} = this.state;
+    if (selected) this.setState({selected: false});
+    else this.setState({selected: true});
   }
-  render() {
-    const { selected } = this.state;
-    const {bankList = [
-      {logo_uri : '../../../../static/img/gd.png', name : '广大银行'},
-      {logo_uri : '../../../../static/img/pf.png', name : '浦发银行'},
-      {logo_uri : '../../../../static/img/zx.png', name : '中信银行'},
-      {logo_uri : '../../../../static/img/zs.png', name : '招商银行'},
-      {logo_uri : '../../../../static/img/gs.png', name : '工商银行'},
-      {logo_uri : '../../../../static/img/gf.png', name : '广发银行'},
-      {logo_uri : '../../../../static/img/xy.png', name : '兴业银行'},
-      {logo_uri : '../../../../static/img/jt.png', name : '交通银行'},
-    ]} = this.props;
+  // 将银行列表在轮播图中分页展示
+  bannerArrange = (bankList, min, max) => {
+    const {bankName} = this.state;
     const bankShow = bankList.map((v, k) => {
-      return <BankList echoData={v} key={k} chooseBank={this.chooseBank} selected={selected} />;
+      if (k >= min && k <= max) {
+        return <BankList echoData={v} key={k} chooseBank={this.chooseBank} selected={v.name == bankName} />;
+      }
     })
+    return bankShow;
+  }
+
+  render() {
+    const {
+      bankList = [
+        {logo_uri: '../../../../static/img/gd.png', name: '广大银行'},
+        {logo_uri: '../../../../static/img/pf.png', name: '浦发银行'},
+        {logo_uri: '../../../../static/img/zx.png', name: '中信银行'},
+        {logo_uri: '../../../../static/img/zs.png', name: '招商银行'},
+        {logo_uri: '../../../../static/img/gs.png', name: '工商银行'},
+        {logo_uri: '../../../../static/img/gf.png', name: '广发银行'},
+        {logo_uri: '../../../../static/img/xy.png', name: '兴业银行'},
+        {logo_uri: '../../../../static/img/jt.png', name: '交通银行'},
+        {logo_uri: '../../../../static/img/zs.png', name: '招商银行'},
+        {logo_uri: '../../../../static/img/gs.png', name: '工商银行'},
+        {logo_uri: '../../../../static/img/gf.png', name: '广发银行'},
+        {logo_uri: '../../../../static/img/gd.png', name: '广大银行'},
+        {logo_uri: '../../../../static/img/pf.png', name: '浦发银行'},
+        {logo_uri: '../../../../static/img/zx.png', name: '中信银行'},
+        {logo_uri: '../../../../static/img/gf.png', name: '广发银行'},
+        {logo_uri: '../../../../static/img/xy.png', name: '兴业银行'},
+        {logo_uri: '../../../../static/img/jt.png', name: '交通银行'},
+        {logo_uri: '../../../../static/img/xy.png', name: '兴业银行'},
+        {logo_uri: '../../../../static/img/jt.png', name: '交通银行'},
+        {logo_uri: '../../../../static/img/gd.png', name: '广大银行'},
+        {logo_uri: '../../../../static/img/pf.png', name: '浦发银行'},
+        {logo_uri: '../../../../static/img/zx.png', name: '中信银行'},
+        {logo_uri: '../../../../static/img/zs.png', name: '招商银行'},
+        {logo_uri: '../../../../static/img/gs.png', name: '工商银行'},
+      ]
+    } = this.props;
     return (
       <div className={styles.container}>
-        <Header title="积分兑换" right="兑换记录" />
+        <Header title="积分兑换" right="兑换记录"/>
         <div className={styles.chooseBank}>选择银行</div>
         <div className={styles.bankList}>
           <Carousel
             autoplay={false}
             infinite
-            dotStyle={{ width: '0.1rem', height: '0.1rem', borderRadius: '50%', background: '#BBBBBB' }}
-            dotActiveStyle={{ width: '0.1rem', height: '0.1rem', borderRadius: '50%', background: '#3399FF' }}
+            dotStyle={{width: '0.1rem', height: '0.1rem', borderRadius: '50%', background: '#BBBBBB'}}
+            dotActiveStyle={{width: '0.1rem', height: '0.1rem', borderRadius: '50%', background: '#3399FF'}}
           >
             <div className={styles.bankListShow}>
               <div className={styles.content}>
-                {bankShow}
+                {this.bannerArrange(bankList, 0, 7)}
               </div>
             </div>
-            <div style={{ background: 'green' }} className={styles.bankListShow}>
-              2222
+            <div className={styles.bankListShow}>
+              <div className={styles.content}>
+                {this.bannerArrange(bankList, 8, 15)}
+              </div>
             </div>
-            <div style={{ background: 'blue' }} className={styles.bankListShow}>
-              3333
+            <div className={styles.bankListShow}>
+              <div className={styles.content}>
+                {this.bannerArrange(bankList, 16, 23)}
+              </div>
             </div>
           </Carousel>
         </div>
         <div className={styles.interval}>.</div>
         <div className={styles.bankDetail}>
-          <img src="../../../../static/img/zs.png" alt="招商银行" />
+          <img src="../../../../static/img/zs.png" alt="招商银行"/>
           <span className={styles.name}>招商银行</span>
           <div className={styles.exchangeInfo}>
             每
@@ -81,24 +120,24 @@ export default class Home extends Component {
               <div className={styles.columnSecond}>银行商品</div>
               <div className={styles.columnThird}>可换购Pluss刷卡金</div>
             </div>
-            <div className={styles.row} style={{ fontSize: '0.26rem' }}>
+            <div className={styles.row} style={{fontSize: '0.26rem'}}>
               <div className={styles.columnFirst}>30000</div>
               <div className={styles.columnSecond}>100元沃尔玛购物券</div>
-              <div className={styles.columnThird} style={{ color: '#3399FF' }}>30元刷卡金</div>
+              <div className={styles.columnThird} style={{color: '#3399FF'}}>30元刷卡金</div>
             </div>
             <div className={styles.row}>
               <div className={styles.columnFirst}>50000</div>
               <div className={styles.columnSecond}>500元沃尔玛购物券</div>
-              <div className={styles.columnThird} style={{ color: '#3399FF' }}>50元刷卡金</div>
+              <div className={styles.columnThird} style={{color: '#3399FF'}}>50元刷卡金</div>
             </div>
           </div>
         </div>
         <div className={styles.interval}>.</div>
         <div className={styles.cashFlow}>
           <span className={styles.text}>兑换流程</span>
-          <img src={process} alt="兑换流程" className={styles.process} />
+          <img src={process} alt="兑换流程" className={styles.process}/>
         </div>
-        <div className={styles.interval} style={{ height: '0.28rem' }}>.</div>
+        <div className={styles.interval} style={{height: '0.28rem'}}>.</div>
         <div className={styles.submit}>立即兑换</div>
       </div>
     );
--- src/page/home/components/BankList.jsx
+++ src/page/home/components/BankList.jsx
@@ -7,13 +7,13 @@ export default class BankList extends Component {
     const { echoData = {}, chooseBank = () => {} , selected = false } = this.props;
     const { logo_uri = '../../../../static/img/new/gd.png', name = '广大银行' } = echoData;
     return (
-      <div className={styles.bankContent} onClick={chooseBank}>
+      <div className={styles.bankContent} onClick={() => { chooseBank(name); }}>
         <div className={styles.bankIcon}>
           <img src={logo_uri} alt={name} className={styles.bankIconImg} />
           <div className={styles.bankIconText}>
             {name}
           </div>
-          <img src={checked} alt="" className={`${selected ? styles.selected:styles.cancel}`} />
+          <img src={checked} alt="" className={`${selected ? styles.selected :  styles.cancel}`} />
         </div>
       </div>
     );
